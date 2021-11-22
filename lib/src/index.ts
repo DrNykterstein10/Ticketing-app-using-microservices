ab140345d4a71dfb7eea345f22d393580fcf599d
Piyush Rai <piyrai1@publicisgroupe.net>
2021-11-22 18:08:52 +0530

    Updates

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -3,6 +3,7 @@ export * from "./errors/customError";
 export * from "./errors/databaseConnectionError";
 export * from "./errors/notAuthorizedError";
 export * from "./errors/requestValidationError";
+export * from "./errors/notFoundError";
 
 export * from "./middleware/currentUserMiddleware";
 export * from "./middleware/errorHandler";
