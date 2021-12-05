e549257d6bd120dbb50b8cee650f84b41d3915ee
Piyush Rai <piyrai1@publicisgroupe.net>
2021-12-05 23:18:23 +0530

    Updates

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -9,3 +9,9 @@ export * from "./middleware/currentUserMiddleware";
 export * from "./middleware/errorHandler";
 export * from "./middleware/requireAuth";
 export * from "./middleware/validateRequest";
+
+export * from "./events/baseListener";
+export * from "./events/basePublisher";
+export * from "./events/subjects";
+export * from "./events/ticketCreatedEvent";
+export * from "./events/ticketUpdatedEvent";
